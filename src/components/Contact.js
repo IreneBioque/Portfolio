15f16398fe454507e543d6c22e789b3cb652a396
IreneBioque <irenebioque@gmail.com>
2021-11-02 14:15:02 +0100

    add component constact

---
+++ src/components/Contact.js
@@ -0,0 +1,57 @@
+const Contact = () => {
+  return (
+    <section id='contact'>
+      <article class='section'>
+        <h2 class='section__title'>Contacto</h2>
+        <p class='section__paragraph'>
+          Si quieres contactar conmigo, tienes mi correo y todas mis redes
+          sociales abajo.
+        </p>
+        <ul class='contact_ul'>
+          <li class='contact_ul--li'>
+            <a
+              href='mailto:irenebioque@gmail.com'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <i class='far fa-envelope'></i>
+            </a>
+          </li>
+          <li class='contact_ul--li'>
+            <a
+              href='https://github.com/IreneBioque'
+              title='Link to Github Profile'
+              target='_blank'
+              rel='noreferrer'
+            >
+              {' '}
+              <i class='fab fa-github'></i>
+            </a>
+          </li>
+          <li class='contact_ul--li'>
+            <a
+              href='https://www.linkedin.com/in/irenebioquemu%C3%B1oz/'
+              title='Link to Linkedin Profile'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <i class='fab fa-linkedin'></i>
+            </a>
+          </li>
+          <li class='contact_ul--li'>
+            <a
+              href='https://twitter.com/Irenillab'
+              title='Link to Twitter Profile'
+              target='_blank'
+              rel='noreferrer'
+            >
+              {' '}
+              <i class='fab fa-twitter'></i>
+            </a>
+          </li>
+        </ul>
+      </article>
+    </section>
+  );
+};
+export default Contact;
