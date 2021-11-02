806fa91a1304e113594e22ef2189005367a797d3
IreneBioque <irenebioque@gmail.com>
2021-11-02 16:34:40 +0100

    add technologies in image

--- src/components/WorkArticle.js
+++ src/components/WorkArticle.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
 const WorkArticle = (props) => {
-  const listTech = props.list;
-  const technologies = listTech.map((data) => {
-    return <li className='list__element'>{data}</li>;
+  const [tech, setTech] = useState('');
+  const technologies = props.list.map((data) => {
+    return <span className='listTech'>{data}</span>;
   });
+  const handleTech = () => {
+    setTech(technologies);
+  };
+  const handleTechOut = () => {
+    setTech('');
+  };
   return (
     <article class='section__work section'>
       <h3 class='section__work--title'>{props.title}</h3>
       <p class='section__work--parrafo'>{props.description}</p>
-      <ul class='section__work--ul'>{technologies}</ul>
+      {/* <ul class='section__work--ul'>{technologies}</ul> */}
       <div class='section__work--links'>
         <a href={props.hrefProyect} target='_blank' rel='noreferrer'>
           Abrir proyecto
@@ -21,11 +30,16 @@ const WorkArticle = (props) => {
           <i class='fab fa-github'></i>
         </a>
       </div>
-      <img
-        src={props.photo}
-        alt={props.altImage}
-        class='section__work--image'
-      />
+      <div class='container'>
+        <img
+          src={props.photo}
+          alt={props.altImage}
+          class='section__work--image'
+          onMouseOver={handleTech}
+          onMouseLeave={handleTechOut}
+        />
+        <p class='centrado'>{tech}</p>
+      </div>
     </article>
   );
 };
