71ba63786fb78c0b3acf002836341679da8008e6
IreneBioque <irenebioque@gmail.com>
2021-11-02 14:15:16 +0100

    add component footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer class='footer'>
+      <a href='#top' class='back-to-top' title='Back to Top'>
+        <i class='far fa-arrow-alt-circle-up'></i>
+      </a>
+      <p>&copy; Irene Bioque Muñoz 2021</p>
+    </footer>
+  );
+};
+export default Footer;
