4f064a6d9aebc81c72874acf392004b07728aa50
IreneBioque <irenebioque@gmail.com>
2021-11-04 23:48:28 +0100

    add photo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,18 @@
+import PersonalPhoto from '../images/irene-foto.jpg';
+
 const Hero = () => {
   return (
     <div class='hero'>
-      <h1 class='hero__title'>Irene Bioque Muñoz</h1>
-      <p class='hero__paragraph'>
-        Desarrolladora frontend en busca de mi primer trabajo.
-      </p>
+      <div>
+        <img src={PersonalPhoto} alt='foto Irene' class='image_about' />
+      </div>
+
+      <div>
+        <h1 class='hero__title'>Irene Bioque Muñoz</h1>
+        <p class='hero__paragraph anim-typewriter'>
+          Desarrolladora frontend en busca de mi primer trabajo.
+        </p>
+      </div>
     </div>
   );
 };
