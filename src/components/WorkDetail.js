d1f9ab1fabba3eea82037b293d92f7f516695493
IreneBioque <irenebioque@gmail.com>
2021-11-04 23:49:15 +0100

    clean code and add props data.json

--- src/components/WorkDetail.js
+++ src/components/WorkDetail.js
@@ -1,11 +1,31 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import '../styles/layout/_workDetail.scss';
+import PhotoProyect1 from '../images/roll-the-dices.jpg';
+import PhotoProyect2 from '../images/rick-y-morty.png';
+import PhotoProyect3 from '../images/serieteca.jpg';
+import PhotoProyect4 from '../images/guess-the-number.jpg';
+import PhotoProyect5 from '../images/web-layout.jpg';
 const WorkDetail = (props) => {
-  // const technologies = props.tech.map((data) => {
-  //   return <li>{data}</li>;
-  // });
+  const technologies = props.proyect.tech.map((data) => {
+    return <li>{data}</li>;
+  });
+  // const [image, setImage] = useState('');
+  // if (props.proyect.id === 1) {
+  //   setImage(PhotoProyect1);
+  // } else if (props.proyect.id === 2) {
+  //   setImage(PhotoProyect2);
+  // } else if (props.proyect.id === 3) {
+  //   setImage(PhotoProyect3);
+  // } else if (props.proyect.id === 4) {
+  //   setImage(PhotoProyect4);
+  // } else if (props.proyect.id === 5) {
+  //   setImage(PhotoProyect5);
+  // } else {
+  //   setImage('');
+  // }
   return (
-    <div className='detail'>
+    <div className='detail' key={props.proyect.id}>
       <h1 className='titleDetail'>{props.proyect.name}</h1>
       <Link to='/'>
         <p className='Link'>
@@ -22,14 +42,14 @@ const WorkDetail = (props) => {
       </a>
       <div className='workDetail'>
         <img
-          src={props.PhotoProyect}
+          src={props.proyect.img}
           alt={props.title}
           className='workDetail__image'
         />
         <h3 className='workDetail__title'>Sobre este proyecto</h3>
         <p className='workDetail__paragraph'>{props.proyect.about}</p>
         <h3 className='workDetail__title'>Tecnologías</h3>
-        <ul className='workDetail__list'>{props.proyect.tech}</ul>
+        <ul className='workDetail__list'>{technologies}</ul>
         <h3 className='workDetail__title'>Github</h3>
         <p className='workDetail__paragraph'>
           Si quieres colaborar, darme alguna idea, ver el código del proyecto te
