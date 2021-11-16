8c6bfe3789c70e7addacfd18d07b9e90c1689c7b
IreneBioque <irenebioque@gmail.com>
2021-11-16 13:40:01 +0100

    link twitter

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,52 +1,52 @@
 const Contact = () => {
   return (
-    <section id='contact'>
-      <article className='section'>
-        <h2 className='section__title'>Contacto</h2>
-        <p className='section__paragraph'>
+    <section id="contact">
+      <article className="section">
+        <h2 className="section__title">Contacto</h2>
+        <p className="section__paragraph">
           Si quieres contactar conmigo, tienes mi correo y todas mis redes
           sociales abajo.
         </p>
-        <ul className='contact_ul'>
-          <li className='contact_ul--li'>
+        <ul className="contact_ul">
+          <li className="contact_ul--li">
             <a
-              href='mailto:irenebioque@gmail.com'
-              target='_blank'
-              rel='noreferrer'
+              href="mailto:irenebioque@gmail.com"
+              target="_blank"
+              rel="noreferrer"
             >
-              <i className='far fa-envelope'></i>
+              <i className="far fa-envelope"></i>
             </a>
           </li>
-          <li className='contact_ul--li'>
+          <li className="contact_ul--li">
             <a
-              href='https://github.com/IreneBioque'
-              title='Link to Github Profile'
-              target='_blank'
-              rel='noreferrer'
+              href="https://github.com/IreneBioque"
+              title="Link to Github Profile"
+              target="_blank"
+              rel="noreferrer"
             >
-              {' '}
-              <i className='fab fa-github'></i>
+              {" "}
+              <i className="fab fa-github"></i>
             </a>
           </li>
-          <li className='contact_ul--li'>
+          <li className="contact_ul--li">
             <a
-              href='https://www.linkedin.com/in/irenebioque/'
-              title='Link to Linkedin Profile'
-              target='_blank'
-              rel='noreferrer'
+              href="https://www.linkedin.com/in/irenebioque/"
+              title="Link to Linkedin Profile"
+              target="_blank"
+              rel="noreferrer"
             >
-              <i className='fab fa-linkedin'></i>
+              <i className="fab fa-linkedin"></i>
             </a>
           </li>
-          <li className='contact_ul--li'>
+          <li className="contact_ul--li">
             <a
-              href='https://twitter.com/Irenillab'
-              title='Link to Twitter Profile'
-              target='_blank'
-              rel='noreferrer'
+              href="https://twitter.com/BioqueDev"
+              title="Link to Twitter Profile"
+              target="_blank"
+              rel="noreferrer"
             >
-              {' '}
-              <i className='fab fa-twitter'></i>
+              {" "}
+              <i className="fab fa-twitter"></i>
             </a>
           </li>
         </ul>
