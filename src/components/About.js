ec645d16293813abb797d8ef224238d232ecc1b7
Irene Bioque <irenebioque@gmail.com>
2022-02-24 18:10:51 +0100

    fixed about me

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,12 @@ const About = () => {
       <article className="section section__about">
         <h2 className="section__title">Sobre mi</h2>
         <p className="section__paragraph">
-    
+          Soy una joven programadora con muchas ganas de empezar una nueva
+          aventura en el mundo digital. Durante años, me he formado en distintas
+          ramas, debido a mi gran curiosidad, encontrando mi pasión en crear
+          aplicaciones y proyectos para el mundo digital. Soy una persona capaz
+          de adaptarse a cualquier circunstancia, sin miedo a equivocarme y
+          proponer soluciones.
         </p>
       </article>
     </section>
