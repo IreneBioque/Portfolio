a685fdf4600f79b10f65387fc5e3e26f60733b92
IreneBioque <irenebioque@gmail.com>
2022-03-05 20:27:58 +0100

    format code

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
 // Fichero src/components/App.js
-import {  useState } from 'react';
-import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import { useState } from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 
-import '../styles/App.scss';
-import Header from './Header';
-import Hero from './Hero';
-import WorkList from './WorkList';
-import About from './About';
-import Contact from './Contact';
-import Technologies from './Technologies';
-import WorkDetail from './WorkDetail';
+import "../styles/App.scss";
+import Header from "./Header";
+import Hero from "./Hero";
+import WorkList from "./WorkList";
+import About from "./About";
+import Contact from "./Contact";
+import Technologies from "./Technologies";
+import WorkDetail from "./WorkDetail";
 // import Footer from './Footer';
 
-import Data from '../data/data.json';
+import Data from "../data/data.json";
 const App = () => {
   const [data, setData] = useState(Data);
   const datafiltered = data.proyects
@@ -29,16 +29,16 @@ const App = () => {
       };
     })
     .reverse();
-  const routeData = useRouteMatch('/proyect/:id');
-  const ProyectId = routeData !== null ? routeData.params.id : '';
+  const routeData = useRouteMatch("/proyect/:id");
+  const ProyectId = routeData !== null ? routeData.params.id : "";
   const selectedProyect = datafiltered.find(
     (proyect) => proyect.id === parseInt(ProyectId)
   );
   return (
     <div>
-      <main className='main'>
+      <main className="main">
         <Switch>
-          <Route path='/' exact>
+          <Route path="/" exact>
             <Header />
             <Hero />
             <WorkList data={datafiltered} />
@@ -47,8 +47,8 @@ const App = () => {
             <Technologies />
           </Route>
 
-          <Route path='/proyect/:id'>
-            <section className='characterDetail'>
+          <Route path="/proyect/:id">
+            <section className="characterDetail">
               <WorkDetail proyect={selectedProyect} />
             </section>
           </Route>
